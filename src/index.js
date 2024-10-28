86af3329c8a92efe9ca49431933e6f6378dd33d7
Bruno <bruno@zgen.hu>
2024-10-28 16:34:45 +0200

    change to /api base route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use("/", routes);
+app.use("/api", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 async function start() {
